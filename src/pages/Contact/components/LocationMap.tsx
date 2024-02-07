ba0deda7424d8c7c814157e9f57ae24bdfc0e98c
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-02-07 18:19:49 +0100

    add map

---
+++ src/pages/Contact/components/LocationMap.tsx
@@ -0,0 +1,16 @@
+const LocationMap = () => {
+  return (
+    <div className="mx-auto w-full">
+      <iframe
+        className="w-full"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2599.051764694555!2d23.5093491!3d49.3511688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473a4ea0fead949b%3A0x482b5c5141cd9ef5!2sLviv%20Regional%20Academic%20Music%20and%20Drama%20Theatre%20named%20by%20Yuriy%20Drohobych!5e0!3m2!1spl!2spl!4v1707302576865!5m2!1spl!2spl"
+        width="600"
+        height="450"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default LocationMap;
