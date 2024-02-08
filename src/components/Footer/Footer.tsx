a4212d9dfdcda4146b002a8522d3a9c48e4e37be
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-02-08 19:34:03 +0100

    change size of logo Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-accent text-light">
       <div className="content grid grid-cols-1 items-end justify-between gap-10 overflow-hidden py-8 leading-8 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4">
         <Logo
-          size={{ width: 130, height: 130 }}
+          size={{ width: 110, height: 110 }}
           className="-translate-x-10 scale-75 lg:translate-x-0 lg:scale-100"
         />
         <Contact />
